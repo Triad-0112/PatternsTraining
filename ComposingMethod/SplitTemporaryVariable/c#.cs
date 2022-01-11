238b5904e13fb1b826927f180d77c3105be083d0
Panji Tri Wahyudi <richard.kennedy11316@gmail.com>
2022-01-11 11:04:49 +0700

    Added Split Temporary Variable

---
+++ ComposingMethod/SplitTemporaryVariable/c#.cs
@@ -0,0 +1,31 @@
+//Case
+double CalculateTotal() 
+{
+  double basePrice = quantity * itemPrice;
+  
+  if (basePrice > 1000) 
+  {
+    return basePrice * 0.95;
+  }
+  else 
+  {
+    return basePrice * 0.98;
+  }
+}
+
+//Solution
+double CalculateTotal() 
+{
+  if (BasePrice() > 1000) 
+  {
+    return BasePrice() * 0.95;
+  }
+  else 
+  {
+    return BasePrice() * 0.98;
+  }
+}
+double BasePrice() 
+{
+  return quantity * itemPrice;
+}
---
+++ ComposingMethod/SplitTemporaryVariable/java.java
@@ -0,0 +1,23 @@
+//Problem
+double calculateTotal() {
+  double basePrice = quantity * itemPrice;
+  if (basePrice > 1000) {
+    return basePrice * 0.95;
+  }
+  else {
+    return basePrice * 0.98;
+  }
+}
+
+//Solution
+double calculateTotal() {
+  if (basePrice() > 1000) {
+    return basePrice() * 0.95;
+  }
+  else {
+    return basePrice() * 0.98;
+  }
+}
+double basePrice() {
+  return quantity * itemPrice;
+}
